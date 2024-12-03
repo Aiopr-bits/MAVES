3db0152baa8694a14cbedef35fe1965778a3ff33
曾志勇 <mailzengzhiyong@gmail.com>
2024-12-03 16:14:54 +0800

    feat:添加vtk库，新增煮茶黄口布局

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "WorkSpaceWindow.h"
+#include "MainWindow.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    WorkSpaceWindow w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
