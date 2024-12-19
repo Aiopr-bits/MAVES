f7e94e51d23797f0d47e76c58f7528743dd31b86
曾志勇 <mailzengzhiyong@gmail.com>
2024-12-19 11:12:00 +0800

    feat:限制求解计算子页面的输入框的输入格式

--- src/FormRun.cpp
+++ src/FormRun.cpp
@@ -1,11 +1,24 @@
 #include "FormRun.h"
+#include <QRegularExpressionValidator> 
 
 FormRun::FormRun(QWidget *parent)
 	: QWidget(parent)
 	, ui(new Ui::FormRunClass())
 {
 	ui->setupUi(this);
+
 	ui->label_6->hide();
+
+	// 设置 lineEdit 只支持整数和小数输入
+	QRegularExpression regExp("^-?\\d*\\.?\\d+([eE][-+]?\\d+)?$");
+	QRegularExpressionValidator* validator = new QRegularExpressionValidator(regExp, this);
+	ui->lineEdit->setValidator(validator);
+	ui->lineEdit_2->setValidator(validator);
+	ui->lineEdit_3->setValidator(validator);
+
+	// 设置 lineEdit_4 只支持正整数输入
+	QIntValidator* intValidator = new QIntValidator(1, INT_MAX, this);
+	ui->lineEdit_4->setValidator(intValidator);
 
 	connect(ui->pushButton, &CustomHoverPushButton::cursorEnter, this, &FormRun::cursorEnter);
 	connect(ui->pushButton, &CustomHoverPushButton::cursorLeave, this, &FormRun::cursorLeave);
