82ad5a22d151834377783582d8ac072a72a14615
曾志勇 <mailzengzhiyong@gmail.com>
2025-01-09 10:58:04 +0800

    feat:增加圆角label类

---
+++ src/CustomRoundedImageLabel.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <QLabel>
+#include <QPainter>
+#include <QPixmap>
+#include <QPainterPath>
+
+class CustomRoundedImageLabel : public QLabel {
+    Q_OBJECT
+
+public:
+    explicit CustomRoundedImageLabel(QWidget* parent = nullptr) : QLabel(parent) {}
+
+    void setPixmap(const QPixmap& pixmap) {
+        m_pixmap = pixmap;
+        update();
+    }
+
+protected:
+    void paintEvent(QPaintEvent* event) override {
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing, true);
+        painter.setRenderHint(QPainter::SmoothPixmapTransform, true);
+
+        QPainterPath path;
+        path.addRoundedRect(rect(), 20, 20); 
+        painter.setClipPath(path);
+
+        if (!m_pixmap.isNull()) {
+            painter.drawPixmap(rect(), m_pixmap);
+        }
+
+        QLabel::paintEvent(event);
+    }
+
+private:
+    QPixmap m_pixmap;
+};
+
