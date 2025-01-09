1c762c2e2ed6517c5f7750f8df57348eb94ea4ec
曾志勇 <mailzengzhiyong@gmail.com>
2025-01-09 17:57:34 +0800

    feat：边界条件子面板（布局）

--- src/FormBoundaryConditions.cpp
+++ src/FormBoundaryConditions.cpp
@@ -9,22 +9,6 @@ FormBoundaryConditions::FormBoundaryConditions(QWidget* parent)
 	listViewModel = new QStandardItemModel(this);
 	ui->listView->setModel(listViewModel);
 	ui->listView->setContextMenuPolicy(Qt::CustomContextMenu);
-
-	// 设置 listView 的样式表
-	ui->listView->setStyleSheet(
-		"QListView {"
-		"    background-color: white;" // 设置元素背景颜色为白色
-		"    border: 1px solid rgb(230, 230, 230);" // 设置边框颜色
-		"    show-decoration-selected: 1;" // 显示选中项的装饰
-		"}"
-		"QListView::item {"
-		"    border-bottom: 1px solid rgb(230, 230, 230);" // 设置元素之间的横线颜色
-		"}"
-		"QHeaderView::section {"
-		"    background-color: rgb(230, 230, 230);" // 设置标题背景颜色
-		"    border: 1px solid rgb(230, 230, 230);" // 设置标题边框颜色
-		"}"
-	);
 }
 
 FormBoundaryConditions::~FormBoundaryConditions()
@@ -41,11 +25,20 @@ void FormBoundaryConditions::onMeshImported()
 	}
 
 	listViewModel->clear();
+
+	QIcon icon("../res/meshBoundary.png");
 	foreach(const QString & name, meshEdgeActorsName) {
-		QStandardItem* item = new QStandardItem(name);
+		QStandardItem* item = new QStandardItem(icon, name); 
 		item->setFlags(item->flags() & ~Qt::ItemIsEditable);
 		item->setSizeHint(QSize(0, 40));
 		listViewModel->appendRow(item);
 	}
+
+	// 计算所有 item 的总高度
+	int totalHeight = 0;
+	for (int i = 0; i < listViewModel->rowCount(); ++i) {
+		totalHeight += ui->listView->sizeHintForRow(i);
+	}
+	ui->listView->setFixedHeight(totalHeight + 2 * ui->listView->frameWidth());
 }
 
