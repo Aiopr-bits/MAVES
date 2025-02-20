21da42667d14676f268bb45f30e77f51ebf4b042
曾志勇 <mailzengzhiyong@gmail.com>
2025-02-20 16:56:12 +0800

    feat：提交x64目录

---
+++ x64/solver/of2406/etc/prefs-8.sh
@@ -0,0 +1,13 @@
+# -*- sh -*-
+# Preferences for system clang-xxx, label-size=32
+export WM_COMPILER_TYPE=system
+
+export WM_COMPILER=Clang80
+export WM_COMPILE_CONTROL="version=8.0 +lld +strict"
+
+export WM_LABEL_SIZE=32
+
+## Use system openmpi
+export WM_MPLIB=SYSTEMOPENMPI
+
+# ----------------------------------------------------------------------------
---
+++ x64/solver/of2406/etc/prefs.csh
@@ -0,0 +1,13 @@
+# -*- sh -*-
+# Preferences for system Clang100, label-size=32
+setenv WM_COMPILER_TYPE system
+
+setenv WM_COMPILER Clang130
+setenv WM_COMPILE_CONTROL "version=13.0"
+setenv WM_LABEL_SIZE 32
+
+## Use ThirdParty openmpi
+## setenv WM_MPLIB OPENMPI
+
+## Use system openmpi
+setenv WM_MPLIB SYSTEMOPENMPI
