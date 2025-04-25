8165ef4ee1a1ec72a8d908878c2f661ac90dc33f
曾志勇 <mailzengzhiyong@gmail.com>
2025-04-25 18:09:06 +0800

    feat:补充文件提交

---
+++ src/FormCellZonesTabWidget.cpp
@@ -0,0 +1,71 @@
+﻿#include "FormCellZonesTabWidget.h"
+#include <QRegularExpressionValidator>
+
+FormCellZonesTabWidget::FormCellZonesTabWidget(const QString& solver, const QString& regionType, const QString& turbulenceType, QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::FormCellZonesTabWidgetClass())
+{
+	ui->setupUi(this);
+
+	ui->tabWidget->setTabVisible(0, false);
+	ui->tabWidget->setTabVisible(1, false);
+	ui->tabWidget->setTabVisible(2, false);
+	ui->tabWidget->setTabVisible(3, false);
+
+	connect(ui->radioButton_12, &QRadioButton::toggled, this, &FormCellZonesTabWidget::on_RadioButton_Toggled);
+	connect(ui->radioButton_13, &QRadioButton::toggled, this, &FormCellZonesTabWidget::on_RadioButton_Toggled);
+	connect(ui->comboBox_20, QOverload<int>::of(&QComboBox::activated), this, &FormCellZonesTabWidget::on_comboBox_20_itemActivated);
+
+	ui->radioButton_12->setChecked(true);
+	on_comboBox_20_itemActivated(0);
+
+	// 输入限制
+	QRegularExpression regex("^-?(\\d+(\\.\\d*)?|\\.\\d+)([eE][-+]?\\d+)?$");
+	QRegularExpressionValidator* validator = new QRegularExpressionValidator(regex, this);
+	const auto lineEdits = this->findChildren<QLineEdit*>();
+	for (QLineEdit* lineEdit : lineEdits) {
+		lineEdit->setValidator(validator);
+	}
+}
+
+FormCellZonesTabWidget::~FormCellZonesTabWidget()
+{
+	delete ui;
+}
+
+void FormCellZonesTabWidget::on_comboBox_20_itemActivated(int index)
+{
+	ui->lineEdit_26->hide();
+	ui->pushButton_5->hide();
+
+	if (index == 0) {
+		ui->lineEdit_26->show(); 
+	}
+	else if (index == 1) {
+		ui->pushButton_5->show();
+	}
+}
+
+void FormCellZonesTabWidget::on_RadioButton_Toggled(bool checked)
+{
+	QRadioButton* senderButton = qobject_cast<QRadioButton*>(sender());
+	if (!senderButton || !checked) return; 
+
+	ui->widget_5->hide();
+	ui->widget_6->hide();
+
+	if (senderButton == ui->radioButton_12)
+	{
+		ui->widget_5->show();
+	}
+	else if (senderButton == ui->radioButton_13)
+	{
+		ui->widget_6->show();
+	}
+}
+
+void FormCellZonesTabWidget::resizeEvent(QResizeEvent* event)
+{
+	int width = ui->tabWidget->width();
+	ui->tabWidget->tabBar()->setFixedWidth(ui->tabWidget->width());
+}
---
+++ src/FormCellZonesTabWidget.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+#include <QWidget>
+#include "ui_FormCellZonesTabWidget.h"
+#include <QTabBar>
+#include <QComboBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class FormCellZonesTabWidgetClass; };
+QT_END_NAMESPACE
+
+class FormCellZonesTabWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	FormCellZonesTabWidget(const QString& solver = "rhoSimpleFoam", const QString& regionType = "fluid", const QString& turbulenceType = "Laminar", QWidget *parent = nullptr);
+	~FormCellZonesTabWidget();
+
+public slots:
+	void resizeEvent(QResizeEvent* event);
+	void on_RadioButton_Toggled(bool checked);
+	void on_comboBox_20_itemActivated(int index);
+
+public:
+	Ui::FormCellZonesTabWidgetClass *ui;
+};
